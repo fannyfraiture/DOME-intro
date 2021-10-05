2ebc49d088254e9741e4868d048f0571b622ea2f
Fanny Fraiture <ffraiture.fanny@gmail.com>
2021-10-05 12:23:34 +0200

    update

--- 4.Navigate/script.js
+++ 4.Navigate/script.js
@@ -6,3 +6,15 @@ const ol = document.querySelector('ol');
 const li = document.querySelector('ol').children;
 const lastChild = li[li.length - 1];
 ol.insertBefore(lastChild, li[0]);
+
+//Put the h2 of the thirdSection in the secondSection
+//Spécifié l'id des sections dans l'html
+const h2 = document.querySelectorAll("h2");
+const secondSection = h2[1].parentNode;
+const thirdSection = h2[2].parentNode;
+secondSection.insertBefore(h2[2], h2[1]);
+thirdSection.insertBefore(h2[1], thirdSection.firstChild);
+
+//Delete the last section from the DOM, we don't need it anyways
+const section = document.getElementById("third");
+section.remove();
